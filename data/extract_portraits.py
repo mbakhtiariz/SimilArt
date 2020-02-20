d63692ce600ed998eee9f1578023308c655e5d4b
Masoomeh Bakhtiariziabari <mbakhtiariz@gmail.com>
2020-02-20 10:39:33 +0100

    you can dl metadate from url

--- data/extract_portraits.py
+++ data/extract_portraits.py
@@ -22,28 +22,40 @@ def extract_portraits(omni_ds, selected_column, sub_cat_name, src_path, dst_path
             # print(file_name)
 
 
-# dl_or_copy = "dl"  # download directly from given ominarts url
-dl_or_copy = "copy" # copy from pre-downloaded dataset from the local path
+# ------------------------------------------------------------------
 
+# Categories that you want to download:
+selected_column = "artwork_type"  # It contains 512 categories!!
+sub_cat_name = "landscape"
+
+# Flags
+dl_or_copy = "dl"  # download directly from given ominarts url
+# dl_or_copy = "copy" # copy from pre-downloaded dataset from the local path
+metadata_dl_flag = False
+
+# Paths:
 # please modify this line to your local path of the dataset:
 local_root_path = Path(r"C:\Users\mbakh\Desktop\Courses\InfoVis\omni-exploration-project\Datasets\omniarts")
+metadata_path = local_root_path / Path(r'artsight_csvs')
+Path(metadata_path).mkdir(parents=True, exist_ok=True)
 
 if dl_or_copy == "dl":
     # official omniart url of low resolution images:
     src_path = "http://isis-data.science.uva.nl/strezoski/omniart/omniart_v3/data/img_300x/"
+    if metadata_dl_flag == True:
+        urllib.request.urlretrieve("http://isis-data.science.uva.nl/strezoski/omniart/omniart_v3/artsight_csvs/metadatas.csv", metadata_path / Path('metadatas.csv'))
+
 else:
     # please modify this line to your local path of the whole dataset:
-    src_path = local_root_path / "data" / "300x"
+    src_path = local_root_path / "data" /  "300x"
 
-# please modify this line to your local metadata path:
-metadata_path = local_root_path / Path(r'artsight_csvs\metadatas.csv')
-selected_column = "artwork_type"  # 512 categories!!
-sub_cat_name = "portrait"
+metadata_path = metadata_path / Path('metadatas.csv')
 
 omni_ds = pd.read_csv(metadata_path)
 print(omni_ds.columns)
 
 dst_path = local_root_path / "data" / "subsets" / selected_column / sub_cat_name
 Path(dst_path).mkdir(parents=True, exist_ok=True)
+
 
 extract_portraits(omni_ds, selected_column, sub_cat_name, src_path, dst_path, dl_or_copy)

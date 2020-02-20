70747c67993376c179e1eebffa03a7cf7e28fa94
Masoomeh Bakhtiariziabari <mbakhtiariz@gmail.com>
2020-02-20 15:16:51 +0100

    changed the selected subset to download

--- data/download_subset_dataset.py
+++ data/download_subset_dataset.py
@@ -7,53 +7,68 @@ import argparse
 
 
 def extract_portraits(omni_ds, selected_column, sub_cat_name, src_path, dst_path, dl_or_copy):
+    counter = 0
     for index in tqdm(range(len(omni_ds))):
-        if omni_ds[selected_column][index] == sub_cat_name:
-            file_name = str(omni_ds['omni_id'][index]) + ".jpg"
-            # if file_name not in ["11034064.jpg", "11038913.jpg", "11041471.jpg"]:
-            dst = dst_path / file_name
-            if dl_or_copy:
-                # download from the official omniart url of low resolution images:
-                src = src_path + file_name
-                urllib.request.urlretrieve(src, dst)
-            else:
-                # copy from local directory
-                src = src_path / file_name
-                copyfile(src, dst)
-            # print(file_name)
+        if counter < 15000:
+            if omni_ds[selected_column][index] == sub_cat_name:
+                counter += 1
+                file_name = str(omni_ds['omni_id'][index]) + ".jpg"
+                # if file_name not in ["11034064.jpg", "11038913.jpg", "11041471.jpg"]:
+                dst = dst_path / file_name
+                if dl_or_copy:
+                    # download from the official omniart url of low resolution images:
+                    src = src_path + file_name
+                    urllib.request.urlretrieve(src, dst)
+                else:
+                    # copy from local directory
+                    src = src_path / file_name
+                    copyfile(src, dst)
+                # print(file_name)
+        else:
+            break
 
 
 # ------------------------------------------------------------------
 def main(opt):
-    print(opt)
+    print(opt,'\n')
 
+    if not opt.dl_dataset:
+        if opt.complete_dataset == '':
+            print("---- Error: You should specify the path to the pre-dowloaded dataset by using \"--complete_dataset\", since you did not use the \"--dl_dataset option\"")
+            assert False
     # Categories that you want to download:
-    selected_column = "artwork_type"  # It contains 512 categories!!
-    sub_cat_name = "landscape"
+    # selected_column = "artwork_type"  # It contains 520 categories!!
+    # categories = ["landscape",  "accessory", "sword blades", "fencing equipment", "plaster-reproductions", "design"]
+
+    selected_column = "general_type"  # It contains 15 categories!!
+    categories = ['print', 'photograph', 'sculpture', 'design', 'weapons and armor', 'drawing', 'painting', 'craft', 'installation', 'textile']
+
 
     # Paths:
     metadata_path = opt.metadata_root
-    Path(metadata_path).mkdir(parents=True, exist_ok=True)
 
     if opt.dl_dataset:
         # official omniart url of low resolution images:
         src_path = "http://isis-data.science.uva.nl/strezoski/omniart/omniart_v3/data/img_300x/"
         if opt.dl_metadata:
+            Path(metadata_path).mkdir(parents=True, exist_ok=True)
             urllib.request.urlretrieve(
-                "http://isis-data.science.uva.nl/strezoski/omniart/omniart_v3/artsight_csvs/metadatas.csv",
+                "http://isis-data.science.uva.nl/strezoski/omniart/omniart_v3/data/csv/omniart_v3_datadump.csv",
                 metadata_path / Path('metadatas.csv'))
     else:
-        src_path = opt.complete_dataset
+        src_path = Path(opt.complete_dataset)
 
-    metadata_path = metadata_path / Path('metadatas.csv')
+    metadata_path = metadata_path / Path('omniart_v3_datadump.csv')
 
     omni_ds = pd.read_csv(metadata_path)
     print(omni_ds.columns)
 
-    dst_path = Path(opt.data_root) / "subsets" / selected_column / sub_cat_name
-    Path(dst_path).mkdir(parents=True, exist_ok=True)
 
-    extract_portraits(omni_ds, selected_column, sub_cat_name, src_path, dst_path, opt.dl_dataset)
+    for sub_cat_name in categories:
+        dst_path = Path(opt.data_root) / "subset" #"subsets" / selected_column / sub_cat_name
+        Path(dst_path).mkdir(parents=True, exist_ok=True)
+
+        extract_portraits(omni_ds, selected_column, sub_cat_name, src_path, dst_path, opt.dl_dataset)
 
 
 # ---------------------------
@@ -64,7 +79,7 @@ if __name__ == '__main__':
                         help='path to save subset images')
 
     parser.add_argument('--metadata_root', required=True,
-                        help='path to load/save the metadata file')
+                        help='path to load/save the metadata file (omniart_v3_datadump.csv)')
 
     parser.add_argument('--complete_dataset', type=str, default='',
                         help='path of predownload whole dataset')
--- data/run_dl_subset.sh
+++ data/run_dl_subset.sh
@@ -1,1 +1,5 @@
 python download_subset_dataset.py --data_root \path\to\dirctory\to\save\dataset --metadata_root \path\to\dirctory\to\save\or\load\metadata --dl_dataset --dl_metadata
+
+
+# Example
+# python download_subset_dataset.py --data_root C:\Users\mbakh\Desktop\Courses\InfoVis\omni-exploration-project\Datasets\omniarts --metadata_root C:\Users\mbakh\Desktop\Courses\InfoVis\omni-exploration-project\Datasets\omniarts\data\csv --dl_dataset

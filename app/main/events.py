2a9f1ebf044b23698e5fa94fb85d475fe9fe2df9
Kylian van Geijtenbeek <kylian.v.geijtenbeek@hotmail.com>
2020-02-22 12:37:57 +0100

    Frontend-backend connection with socket.io and nearest neighbor querying from js

--- app/main/events.py
+++ app/main/events.py
@@ -1,6 +1,20 @@
 from flask_socketio import emit, send
 from .. import socketio
+import os
+
+from ..features import Features
+
+features_path = os.path.join(os.path.abspath(__file__), r'..\..\data\features.csv')
+features = Features(features_path)
 
 @socketio.on('connect')
 def test_connect():
     print("Connection succesful")
+
+@socketio.on('nearest_neighbors')
+def nearest_neighbors(info):
+    print("Emitting nearest neighbors query")
+    id = info['id']
+    n = info['n']
+    dists, ids = features.nearest(int(id), n)
+    emit('nearest_neighbors_data', {'dists': dists, 'ids': ids})
---
+++ app/static/js/nearest_neighbors.js
@@ -0,0 +1,21 @@
+// Connection needed for the nearest neighbor queries
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function() {
+	console.log('Websocket connected!');
+});
+
+// distances and nearest neighbor ids are stored in variables below
+var dists = []
+var nearest_ids = []
+console.log(nearest_ids);
+
+// Functions needed for the nearest neighbor queries
+function nearest_neighbors(id, n_neighbors) {
+	console.log('Getting nearest neighbors...');
+	socket.emit('nearest_neighbors', {id: id, n: n_neighbors});
+}
+socket.on('nearest_neighbors_data', function(data) {
+	dists = data.dists;
+	nearest_ids = data.ids;
+	console.log(nearest_ids);
+});

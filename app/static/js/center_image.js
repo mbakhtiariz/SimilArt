7451a980b20f850010ab4c4e3fd8846e56a83b23
Iulia <iulia.ionescu@student.uva.nl>
2020-02-27 12:12:48 +0100

    show images on random locations in grid

--- app/static/js/center_image.js
+++ app/static/js/center_image.js
@@ -1,7 +1,14 @@
+var center_image_size = 100;
+var center_image_x = svgwidth * 0.5 * 0.5 - center_image_size / 2;
+var center_image_y = svgheight * 0.5 * 0.5 - center_image_size / 2;
+console.log(center_image_x)
+console.log(center_image_y)
+
+
 function set_center(center_obj, img_id) {
 	var img = new Image();
 	toString(img_id);
-	url = "static/subset/"+img_id+".jpg"
+	url = "static/subset/" + img_id + ".jpg"
 	img.src = url;
 	img_width = img.width;
 	img_height = img.height;
@@ -17,12 +24,11 @@ function set_center(center_obj, img_id) {
 		img_height = image.size;
 	};
 
-
 	center_obj.attr("href", url)
-		.attr("x",  svgwidth*0.5*0.5 - 50)
-		.attr("y", svgwidth*0.25*0.5 - 50)
-		.attr("height", 100)
-		.attr("width", 100)
+		.attr("x",  center_image_x)
+		.attr("y", center_image_y)
+		.attr("height", center_image_size)
+		.attr("width", center_image_size)
 		.attr("id", 'center');
 
 	center_obj

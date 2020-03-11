2df8938b24207bdc8af0ddc595819bf5c9dec9c0
Kylian van Geijtenbeek <kylian.v.geijtenbeek@hotmail.com>
2020-03-11 09:40:08 +0100

    Center image now correctly sized with height/width ratio

--- app/static/js/center_image.js
+++ app/static/js/center_image.js
@@ -1,32 +1,44 @@
-var center_image_x = svgwidth * 0.5 * 0.5 - center_image_size / 2;
-var center_image_y = svgheight * 0.5 * 0.5 - center_image_size / 2;
+var center_image_x;
+var center_image_y;
 
 function set_center(center_obj, img_id) {
+	toString(img_id);
+	var img = new Image();
+	img.onload = function () {
+		img_width = img.width;
+		img_height = img.height;
+
+		var ratio = img_width / img_height;
+		// If image width > height
+		if (ratio > 1) {
+			img_width = center_image_size;
+			img_height = center_image_size / ratio;
+		// If image height > width
+		} else {
+			img_width = center_image_size * ratio;
+			img_height = center_image_size;
+		};
+		set_center_main(center_obj, img_id, img_width, img_height);
+	}
+	const url = "static/subset/" + img_id + ".jpg";
+	img.src = url;
+}
+
+function set_center_main(center_obj, img_id, img_width, img_height) {
 	var img = new Image();
 	toString(img_id);
-	url = "static/subset/" + img_id + ".jpg"
-	img.src = url;
-	img_width = img.width;
-	img_height = img.height;
+	const url = "static/subset/" + img_id + ".jpg";
 
-	var ratio = img_width / img_height;
-	// If image width > height
-	if (ratio > 1) {
-		img_width = similar_image_size;
-		img_height = similar_image_size / ratio;
-	// If image height > width
-	} else {
-		img_width = similar_image_size * ratio;
-		img_height = similar_image_size;
-	};
+	center_image_x = middle_width * 0.5 - img_width / 2;
+	center_image_y = middle_height * 0.5 - img_height / 2;
 
 	var im = center_obj.append("image")
 		.attr("clip-path", "url(#clip)")
 		.attr("href", url)
 		.attr("x",  center_image_x)
 		.attr("y", center_image_y)
-		.attr("height", center_image_size)
-		.attr("width", center_image_size)
+		.attr("height", img_height)
+		.attr("width", img_width)
 		.attr("id", 'center');
 
 	// Add image full resolution url
@@ -34,8 +46,8 @@ function set_center(center_obj, img_id) {
 	modalImg.src = data[url.replace(/^.*[\\\/]/, '').split('.').slice(0, -1).join('.')]['image_url'];
 
 	// Change image to alternate image if full resolution url does not exist
-	modalImg.onerror = function() { 
-		modalImg.src = img.src; 
+	modalImg.onerror = function() {
+		modalImg.src = img.src;
 	}
 
 	// Change information of pop up middle image
@@ -53,26 +65,26 @@ function set_center(center_obj, img_id) {
 	center_obj.append('rect')
 		.attr("x",  center_image_x)
 		.attr("y", center_image_y)
-		.attr("height", center_image_size)
-		.attr("width", center_image_size)
+		.attr("height", img_height)
+		.attr("width", img_width)
 		.attr("filter", "url(#glow)")
 		.attr("id", 'center')
 		.on('click', function(){
-			var modal = document.getElementById("myModal");			
+			var modal = document.getElementById("myModal");
 			modal.style.display = "block";
-			
+
 			// Get the <span> element that closes the modal
 			var span = document.getElementsByClassName("close")[0];
 			// When the user clicks on <span> (x), close the modal
 			span.onclick = function() {
 				modal.style.display = "none";
-			} 
+			}
 			// When the user clicks anywhere outside of the modal, close it
 			window.onclick = function(event) {
 				if (event.target == modal) {
 					modal.style.display = "none";
 				}
-			} 
+			}
 		});
 
 	center_obj

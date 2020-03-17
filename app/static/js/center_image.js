004d9535dc610c44e7fd9f4f9fff8b033d28b05f
mbakhtiariz <mbakhtiariz@gmail.com>
2020-03-17 14:42:03 +0100

    ctr+click for expanding all photos/ modified info

--- app/static/js/center_image.js
+++ app/static/js/center_image.js
@@ -91,19 +91,21 @@ function set_center_main(center_obj, img_id, img_width, img_height) {
 		.attr("filter", "url(#glow)")
 		.attr("id", 'center')
 		.on('click', function(){
-			var modal = document.getElementById("myModal");
-			modal.style.display = "block";
+			if (d3.event.ctrlKey) {	
+				var modal = document.getElementById("myModal");
+				modal.style.display = "block";
 
-			// Get the <span> element that closes the modal
-			var span = document.getElementsByClassName("close")[0];
-			// When the user clicks on <span> (x), close the modal
-			span.onclick = function() {
-				modal.style.display = "none";
-			}
-			// When the user clicks anywhere outside of the modal, close it
-			window.onclick = function(event) {
-				if (event.target == modal) {
+				// Get the <span> element that closes the modal
+				var span = document.getElementsByClassName("close")[0];
+				// When the user clicks on <span> (x), close the modal
+				span.onclick = function() {
 					modal.style.display = "none";
+				}
+				// When the user clicks anywhere outside of the modal, close it
+				window.onclick = function(event) {
+					if (event.target == modal) {
+						modal.style.display = "none";
+					}
 				}
 			}
 		})
